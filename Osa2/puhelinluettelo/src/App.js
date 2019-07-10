e97028959b331a2008b8d835b895123c2758911d
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-07-10 19:13:31 +0300

    Osa2 puhelinluettelo 2.11

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Numbers from "./components/Numbers";
 import Filters from "./components/Filters";
 import Forms from "./components/Forms";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState(""); // nimi inputin arvo
   const [newNumber, setNewNumber] = useState(""); //numero inputin arvo
   const [filterInput, setFilterInput] = useState(""); //filter inputin arvo
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleChangeName = event => {
     let inputFieldValue = event.target.value;

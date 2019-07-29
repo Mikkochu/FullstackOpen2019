f1a17b1118bce0c1b1082f1e83a6a41487a875f9
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-07-29 19:02:31 +0300

    OSA4 blogilista tehtävä 4.4 tehty

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  const allLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  return allLikes;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };

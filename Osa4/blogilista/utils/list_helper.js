757630aaf3f73633cea95027a09c33bcf2d05ec7
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-07-29 22:36:12 +0300

    OSA4 blogilista tehtävä 4.6 tehty

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+var lodash = require("lodash");
+
 const dummy = blogs => {
   return 1;
 };
@@ -21,13 +23,24 @@ const favoriteBlog = blogs => {
       likes: mostLikedBlog.likes
     }
   ];
-  console.log("mostLikedBlogSimplified", mostLikedBlogSimplified);
+  //console.log("mostLikedBlogSimplified", mostLikedBlogSimplified);
 
   return mostLikedBlogSimplified;
+};
+
+const mostBlogs = blogs => {
+  const authorWithMostBlogs = lodash(blogs)
+    .groupBy("author")
+    .map((blog, author) => ({ author, blogs: blog.length }))
+    .maxBy("blogs");
+
+  //console.log("authorWithMostBlogs", authorWithMostBlogs);
+  return authorWithMostBlogs;
 };
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 };

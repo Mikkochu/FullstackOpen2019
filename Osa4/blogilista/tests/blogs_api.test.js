58a7552783e17628e30775eea1da3c37f3b2211e
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-07-30 21:29:47 +0300

    OSA4 tehtävä 4.9 tehty

--- Osa4/blogilista/tests/blogs_api.test.js
+++ Osa4/blogilista/tests/blogs_api.test.js
@@ -10,6 +10,14 @@ test("All blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
+test("Identifiable id is id", async () => {
+  const response = await api.get("/api/blogs");
+
+  const blogIdField = response.body.map(blog => blog.id); //models/blog.js on määritelty id
+  //console.log("blogIDfield", blogIdField);
+  expect(blogIdField).toBeDefined();
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });

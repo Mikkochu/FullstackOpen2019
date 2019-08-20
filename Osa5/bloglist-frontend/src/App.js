eef01cca47ac4de8ba8c50bfb94c88b7b947096c
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-08-20 16:36:17 +0300

    5.1 tehty

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 function App() {
-  return <div className="App">Mikko</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    blogService.getAll().then(response => {
+      setBlogs(response);
+    });
+  }, []);
+
+  const handleLogin = async event => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      });
+      console.log("user", user);
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log("error in login");
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const showBlogs = () => (
+    <div>
+      {blogs.map(blog => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log into application</h2>
+        {loginForm()}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {showBlogs()}
+    </div>
+  );
 }
 
 export default App;

3fc051083f36e0d5efc17a5e683a86e662086f84
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-08-26 19:37:12 +0300

    5.17 tehty

--- Osa5/bloglist-frontend/src/App.test.js
+++ Osa5/bloglist-frontend/src/App.test.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { render, waitForElement } from "@testing-library/react";
+import { render, waitForElement, cleanup } from "@testing-library/react";
 jest.mock("./services/blogs");
 import App from "./App";
 
-const user = {
-  username: "tester",
-  name: "Donald Tester",
-  token: "1231231214"
-};
-
 describe("<App />", () => {
+  afterEach(cleanup);
   test("if no user logged, notes are not rendered", async () => {
     const component = render(<App />);
     component.rerender(<App />);
@@ -23,17 +18,22 @@ describe("<App />", () => {
   });
 
   test("blogs are rendered when user is logged in", async () => {
-    localStorage.setItem("loggedUser", JSON.stringify(user));
+    const user = {
+      username: "tester",
+      name: "Donald Tester",
+      token: "1231231214"
+    };
+
+    localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
 
     const component = render(<App />);
     component.rerender(<App />);
 
-    await waitForElement(() => component.getAllByText("login"));
-
-    //localStorage.setItem("loggedUser", JSON.stringify(user));
-    component.rerender(<App />);
+    await waitForElement(() => component.getAllByText("logout"));
+    component.debug();
 
     const blogsWithLogin = component.container.querySelectorAll(".titleAuthor");
+    //console.log("blogsWithLogin", blogsWithLogin);
     expect(blogsWithLogin.length).toBe(2);
   });
 });
--- Osa5/bloglist-frontend/src/services/__mocks__/blogs.js
+++ Osa5/bloglist-frontend/src/services/__mocks__/blogs.js
@@ -4,23 +4,27 @@ const blogs = [
     author: "Kirjoittaja1",
     url: "www.testblog.com",
     likes: 10,
-    user: {
-      username: "tester",
-      name: "Donald Tester",
-      id: "555555"
-    },
-    id: "666666"
+    user: [
+      {
+        username: "Mikko",
+        name: "Donald Tester",
+        id: "5d482153bd3d6534d8dd8396"
+      }
+    ],
+    id: "5d482127bd3d6534d8dd8395"
   },
   {
     title: "testiBlogi2",
     author: "Kirjoittaja2",
     url: "www.testblog2.com",
     likes: 20,
-    user: {
-      username: "testiKäyttäjä2",
-      name: "testiKäyttäjä2",
-      id: "888888"
-    },
+    user: [
+      {
+        username: "testiKäyttäjä2",
+        name: "testiKäyttäjä2",
+        id: "888888"
+      }
+    ],
     id: "9999999"
   }
 ];
@@ -29,11 +33,10 @@ let token = null;
 
 const setToken = newToken => {
   token = `bearer ${newToken}`;
-  console.log("token", token);
 };
 
 const getAll = () => {
   return Promise.resolve(blogs);
 };
 
-export default { getAll, setToken };
+export default { getAll, setToken, blogs };

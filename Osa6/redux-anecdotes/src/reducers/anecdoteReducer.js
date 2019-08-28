984661e7d8903b53500b8dbbc9c16dde425a4bcb
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-08-28 21:51:40 +0300

    6.3 tehty

---
+++ Osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,42 @@
+const anecdotesAtStart = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time."
+];
+
+//Muista lisätä loput mp
+
+const getId = () => (100000 * Math.random()).toFixed(0);
+
+const asObject = anecdote => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  };
+};
+
+const initialState = anecdotesAtStart.map(asObject);
+
+const reducer = (state = initialState, action) => {
+  //console.log("state now: ", state);
+  //console.log("action", action);
+
+  switch (action.type) {
+    case "VOTE":
+      //console.log("action", action.data);
+      const id = action.data.id;
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id);
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      };
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      );
+    default:
+      return state;
+  }
+};
+
+export default reducer;
